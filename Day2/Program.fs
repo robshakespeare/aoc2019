0e022bef5994f6e1fff6f5cad9e73ac65848ede7
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2019-12-02 10:01:15 +0000

    Day 2 part 1 solved

--- Day2/Program.fs
+++ Day2/Program.fs
@@ -1,8 +1,45 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
+open System.Linq
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let intCodes =
+        File.ReadAllText("input.txt")
+            .Split(',')
+            .Select(fun s -> int(s))
+            .ToArray()
+
+    Array.set intCodes 1 12
+    Array.set intCodes 2 2
+
+    let processIntCodes (intCodes : int[]) =
+        let mutable index = 0
+        while intCodes.[index] <> 99 do
+            let opcode = intCodes.[index]
+            let leftIndex = intCodes.[index + 1]
+            let rightIndex = intCodes.[index + 2]
+            let storageIndex = intCodes.[index + 3]
+
+            let left = intCodes.[leftIndex]
+            let right = intCodes.[rightIndex]
+
+            let result =
+                match opcode with
+                | 1 -> left + right
+                | 2 -> left * right
+                | _ -> raise (InvalidOperationException("Invalid opcode: " + string(opcode)))
+    
+            Array.set intCodes storageIndex result
+
+            index <- index + 4
+    
+        intCodes.[0]
+
+    let result = processIntCodes intCodes
+    
+    printfn "Solution: %s" (string(result))
+
     0 // return an integer exit code
