87f756f499f6998cc5788596a9fdb967bcf151cc
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2019-12-02 12:48:11 +0000

    Refactored to include test, and not use exit break statement for finding solution

--- Day2/Program.fs
+++ Day2/Program.fs
@@ -1,12 +1,12 @@
 ﻿// Learn more about F# at http://fsharp.org
+module Day2
 
 open System
 open System.IO
 open System.Linq
 
-[<EntryPoint>]
-let main argv =
-    let loadIntCodes noun verb =
+type Solver = class
+    static member loadIntCodes noun verb =
         let intCodes =
             File.ReadAllText("input.txt")
                 .Split(',')
@@ -14,10 +14,10 @@ let main argv =
                 .ToArray()
         Array.set intCodes 1 noun
         Array.set intCodes 2 verb
-        
+
         intCodes
 
-    let processIntCodes (intCodes : int[]) =
+    static member processIntCodes (intCodes : int[]) =
         let mutable index = 0
         while intCodes.[index] <> 99 do
             let opcode = intCodes.[index]
@@ -33,21 +33,26 @@ let main argv =
                 | 1 -> left + right
                 | 2 -> left * right
                 | _ -> raise (InvalidOperationException("Invalid opcode: " + string(opcode)))
-    
+
             Array.set intCodes storageIndex result
 
             index <- index + 4
-    
+
         intCodes.[0]
 
-    for noun in 0 .. 99 do
-        for verb in 0 .. 99 do
-            let result = processIntCodes (loadIntCodes noun verb)
+    static member findSolution () =
+        seq {
+                for noun in 0 .. 99 do
+                    for verb in 0 .. 99 ->
+                        (Solver.processIntCodes (Solver.loadIntCodes noun verb), noun, verb)
+            }
+            |> Seq.find (fun (result, noun, verb) -> result = 19690720)
+            |> fun (result, noun, verb) -> 100 * noun + verb
+end
 
-            if result = 19690720 then
-                let solution = 100 * noun + verb
-                printfn "Solution: %s" (string(solution))
+[<EntryPoint>]
+let main _ =
+    let solution = Solver.findSolution ()
+    printfn "Solution: %s" (string(solution))
 
-                exit 0 // return integer exit code indicating success
-
-    1 // return integer exit code indicating failure
+    0 // return integer exit code indicating success
