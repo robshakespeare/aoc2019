412de9e107b79ece90985d0391f4f587efe2eb2f
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2019-12-10 07:11:11 +0000

    Tidy

--- Day10.Tests/Day10SolverTests.cs
+++ Day10.Tests/Day10SolverTests.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Drawing;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -28,29 +26,5 @@ namespace Day10.Tests
             // ASSERT
             part2Result.Should().Be(null);
         }
-
-        //[Test]
-        //public void CalculateAngleBetweenVector_Tests()
-        //{
-        //    // ACT
-        //    var result1 = sut.CalculateAngleBetweenVectors(new PointF(1, 1), new PointF(1, 1));
-        //    var result2 = sut.CalculateAngleBetweenVectors(new PointF(1, 1), new PointF(1, 2));
-        //    var result3 = sut.CalculateAngleBetweenVectors(new PointF(1, 1), new PointF(1, 3));
-
-        //    // ASSERT
-        //    result1.Should().Be(result2);
-        //    result1.Should().Be(result3);
-        //}
-
-        //[Test]
-        //public void CalculateAngleBetweenVector_Tests_2()
-        //{
-        //    // ACT
-        //    var result1 = sut.CalculateAngleBetweenVectors(new PointF(2.1f, 2.1f), new PointF(2.2f, 2.2f));
-        //    var result2 = sut.CalculateAngleBetweenVectors(new PointF(3.1f, 3.1f), new PointF(4.1f, 4.1f));
-
-        //    // ASSERT
-        //    result1.Should().Be(result2);
-        //}
     }
 }
--- Day10/Day10Solver.cs
+++ Day10/Day10Solver.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Drawing;
 using Common;
 
 namespace Day10
@@ -15,27 +13,5 @@ namespace Day10
         {
             return base.SolvePart2(input);
         }
-
-        //public double CalculateAngleBetweenVectors(PointF v1, PointF v2)
-        //{
-        //    // angle(v1, v2) = acos( (v1x * v2x + v1y * v2y) / (sqrt(v1x^2+v1y^2) * sqrt(v2x^2+v2y^2)) )
-
-        //    return (v1.X * v2.X + v1.Y * v2.Y);
-
-        //    return Math.Acos(
-        //        (v1.X * v2.X + v1.Y * v2.Y) /
-        //        (Math.Sqrt(Math.Pow(v1.X, 2) + Math.Pow(v1.Y, 2)) * Math.Sqrt(Math.Pow(v2.X, 2) + Math.Pow(v2.Y, 2))));
-        //}
-
-        //public PointF Normalise(PointF v)
-        //{
-        //    return v / 2.5f;
-        //}
-
-        ////public PointF NormalizeVector(PointF v)
-        ////{
-        ////    var length = CalculateLengthOfVector(v);
-        ////    return new PointF(v.X / length, v.Y / length);
-        ////}
     }
 }
