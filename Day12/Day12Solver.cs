b4ae6e696c7ad14c61e557655508d8779cbf5af4
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2019-12-11 07:27:20 +0000

    Added day 12

---
+++ Day12/Day12Solver.cs
@@ -0,0 +1,19 @@
+using Common;
+
+namespace Day12
+{
+    public class Day12Solver : SolverReadAllLines
+    {
+        public static void Main() => new Day12Solver().Run();
+
+        public override long? SolvePart1(string[] input)
+        {
+            return base.SolvePart1(input);
+        }
+
+        public override long? SolvePart2(string[] input)
+        {
+            return base.SolvePart2(input);
+        }
+    }
+}
