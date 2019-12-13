1c183cd7cdace9dd0a3715495b1e996b71687618
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2019-12-13 17:42:35 +0000

    Ready for day 14

---
+++ Day14.Tests/Day14SolverTests.cs
@@ -0,0 +1,44 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Day14.Tests
+{
+    public class Day14SolverTests
+    {
+        private readonly Day14Solver sut = new Day14Solver();
+
+        [Test]
+        public void Part1ReTest()
+        {
+            // ACT
+            var part1Result = sut.SolvePart1();
+
+            // ASSERT
+            part1Result.Should().Be(null);
+        }
+
+        [Test]
+        public void Part2ReTest()
+        {
+            // ACT
+            var part2Result = sut.SolvePart2();
+
+            // ASSERT
+            part2Result.Should().Be(null);
+        }
+
+        /*
+        [Test]
+        public void ExampleTest()
+        {
+            const string input = @"";
+
+            // ACT
+            var result = sut.SolvePart1(input.ReadAllLines());
+
+            // ASSERT
+            result.Should().Be(xyz);
+        }
+        */
+    }
+}
---
+++ Day14/Day14Solver.cs
@@ -0,0 +1,19 @@
+using Common;
+
+namespace Day14
+{
+    public class Day14Solver : SolverReadAllLines
+    {
+        public static void Main() => new Day14Solver().Run();
+
+        public override long? SolvePart1(string[] input)
+        {
+            return base.SolvePart1(input);
+        }
+
+        public override long? SolvePart2(string[] input)
+        {
+            return base.SolvePart2(input);
+        }
+    }
+}
