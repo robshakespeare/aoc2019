e658281b4c22c700e06b0440b25614627b249fef
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2019-12-14 19:47:01 +0000

    Minor template update

--- Template/AoC/DayXXX/DayXXXSolver.cs
+++ Template/AoC/DayXXX/DayXXXSolver.cs
@@ -2,14 +2,14 @@ using Common;
 
 namespace AoC.DayXXX
 {
-    public class DayXXXSolver : SolverReadAllLines
+    public class DayXXXSolver : SolverReadAllText
     {
-        public override long? SolvePart1(string[] input)
+        public override long? SolvePart1(string input)
         {
             return base.SolvePart1(input);
         }
 
-        public override long? SolvePart2(string[] input)
+        public override long? SolvePart2(string input)
         {
             return base.SolvePart2(input);
         }
