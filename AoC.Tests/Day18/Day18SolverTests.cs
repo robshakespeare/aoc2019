e667deb797d94a632be36bcc410d64cd197e171c
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2019-12-17 14:06:13 +0000

    Day 18 ready

---
+++ AoC.Tests/Day18/Day18SolverTests.cs
@@ -0,0 +1,31 @@
+using AoC.Day18;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AoC.Tests.Day18
+{
+    public class Day18SolverTests
+    {
+        private readonly Day18Solver sut = new Day18Solver();
+
+        [Test]
+        public void Part1ReTest()
+        {
+            // ACT
+            var part1Result = sut.SolvePart1();
+
+            // ASSERT
+            part1Result.Should().Be(null);
+        }
+
+        [Test]
+        public void Part2ReTest()
+        {
+            // ACT
+            var part2Result = sut.SolvePart2();
+
+            // ASSERT
+            part2Result.Should().Be(null);
+        }
+    }
+}
--- AoC/Program.cs
+++ AoC/Program.cs
@@ -2,6 +2,6 @@ namespace AoC
 {
     public class Program
     {
-        public static void Main() => new Day17.Day17Solver().Run();
+        public static void Main() => new Day18.Day18Solver().Run();
     }
 }
