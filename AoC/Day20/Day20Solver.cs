2490a58d24b67b180d5170d4d3a7c72082f761cc
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2019-12-19 18:40:34 +0000

    Day 20 ready

---
+++ AoC/Day20/Day20Solver.cs
@@ -0,0 +1,17 @@
+using Common;
+
+namespace AoC.Day20
+{
+    public class Day20Solver : SolverReadAllText
+    {
+        public override long? SolvePart1(string input)
+        {
+            return base.SolvePart1(input);
+        }
+
+        public override long? SolvePart2(string input)
+        {
+            return base.SolvePart2(input);
+        }
+    }
+}
--- AoC/Program.cs
+++ AoC/Program.cs
@@ -2,6 +2,6 @@ namespace AoC
 {
     public class Program
     {
-        public static void Main() => new Day19.Day19Solver().Run();
+        public static void Main() => new Day20.Day20Solver().Run();
     }
 }
