8d6f6337febdf84e4cc80a741c83f217d48408a2
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2019-12-19 14:51:12 +0000

    Day 19, part 1, done

--- AoC/Day19/Day19Solver.cs
+++ AoC/Day19/Day19Solver.cs
@@ -1,17 +1,36 @@
+using System;
+using System.Linq;
 using Common;
+using Common.Extensions;
+using Common.IntCodes;
 
 namespace AoC.Day19
 {
     public class Day19Solver : SolverReadAllText
     {
-        public override long? SolvePart1(string input)
+        private readonly IntCodeComputer intCodeComputer = new IntCodeComputer();
+
+        public override long? SolvePart1(string inputProgram)
         {
-            return base.SolvePart1(input);
+            var lines =
+                Enumerable.Range(0, 50)
+                    .Select(y => string.Join(
+                        "",
+                        Enumerable.Range(0, 50)
+                            .Select(x => intCodeComputer.ParseAndEvaluate(inputProgram, x, y).LastOutputValue == 1 ? "#" : ".")))
+                    .ToReadOnlyArray();
+
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            return lines.SelectMany(line => line.ToCharArray()).Count(c => c == '#');
         }
 
-        public override long? SolvePart2(string input)
+        public override long? SolvePart2(string inputProgram)
         {
-            return base.SolvePart2(input);
+            return base.SolvePart2(inputProgram);
         }
     }
 }
