2cfef63ae165911c984c7588e13a77ac21bd4618
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2019-12-19 14:39:18 +0000

    Day 19 ready, bit late tho!

---
+++ AoC/Day19/Day19Solver.cs
@@ -0,0 +1,17 @@
+using Common;
+
+namespace AoC.Day19
+{
+    public class Day19Solver : SolverReadAllText
+    {
+        public override long? SolvePart1(string input)
+        {
+            return base.SolvePart1(input);
+        }
+
+        public override long? SolvePart2(string input)
+        {
+            return base.SolvePart2(input);
+        }
+    }
+}
--- AoC/Program.cs
+++ AoC/Program.cs
@@ -2,6 +2,6 @@ namespace AoC
 {
     public class Program
     {
-        public static void Main() => new Day18.Day18Solver().Run();
+        public static void Main() => new Day19.Day19Solver().Run();
     }
 }
