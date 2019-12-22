559259cf02b20600a4fb39a644fe3e55d40925c7
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2019-12-22 03:15:16 +0000

    Day 22 tidy

--- AoC/Day21/Day21Solver.cs
+++ AoC/Day21/Day21Solver.cs
@@ -14,77 +14,38 @@ namespace AoC.Day21
         private static readonly IntCodeComputer IntCodeComputer = new IntCodeComputer();
 
         public override long? SolvePart1(string intCodeProgram) => Solve(intCodeProgram, @"
-// jump if next tile is a hole:
-NOT A J
+            // jump if next tile is a hole:
+            NOT A J
 
-// jump if 3rd tile in front is a hole:
-NOT C T
-OR T J
+            // jump if 3rd tile in front is a hole:
+            NOT C T
+            OR T J
 
-// only jump if we have a landing:
-AND D J
-WALK");
+            // only jump if we have a landing:
+            AND D J
+            WALK");
 
         public override long? SolvePart2(string intCodeProgram) => Solve(intCodeProgram, @"
-// jump if next tile (A) is a hole:
-NOT A J
+            // jump if next tile (A) is a hole:
+            NOT A J
 
-// jump if 2nd tile (B) in front is a hole:
-NOT B T
-OR T J
+            // jump if 2nd tile (B) in front is a hole:
+            NOT B T
+            OR T J
 
-// jump if 3rd tile (C) in front is a hole:
-NOT C T
-OR T J
+            // jump if 3rd tile (C) in front is a hole:
+            NOT C T
+            OR T J
 
-// only jump if we have a landing (i.e 4th tile == D):
-AND D J
+            // only jump if we have a landing (i.e 4th tile == D):
+            AND D J
 
-// only jump if 5th (E) or 8th (H) are ground
-NOT J T // if we are to jump, T will now be false
-OR E T
-OR H T
-AND T J
-RUN");
-
-////        public override long? SolvePart2(string intCodeProgram) => Solve(intCodeProgram, @"
-////OR G T
-////AND H T
-////AND I T // by this point T == true, if there is ground at 7th, 8th and 9th
-
-////////AND T J // by this point J == true, if there is the gap
-////////NOT J J // by this point J == true, if there is NO gap
-
-////// jump if 3rd tile in front is a hole, if it is followed by ground at 7th, 8th and 9th
-////// i.e. always jump if 3rd tile is a hole, except for when we have the 3 gap at 7th, 8th and 9th
-////NOT C J
-////AND T J
-
-//////AND E J
-
-//////// jump if 2nd and 5th is hole
-////////NOT B J
-////////NOT E T
-////////AND T J
-
-////// jump if next tile is a hole:
-////NOT A T
-////OR T J
-
-////// jump if 2nd tile is a hole:
-////NOT B T
-////OR T J
-
-//////// jump if 5th is hole, as long as we have landing, and a landing after that for if we need to jump:
-////////NOT E T
-////////AND F T
-////////AND H T
-////////AND I T
-////////OR T J
-
-////// only jump if we have a landing:
-////AND D J
-////RUN");
+            // only jump if 5th (E) or 8th (H) are ground
+            NOT J T // if we are to jump, T will now be false
+            OR E T
+            OR H T
+            AND T J
+            RUN");
 
         private static long? Solve(string intCodeProgram, string springScript)
         {
