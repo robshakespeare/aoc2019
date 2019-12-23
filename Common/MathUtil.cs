8b5e2b2f0a91e09ce3c1ad805e66a2f3c534d7e2
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2019-12-23 17:30:48 +0000

    Replaced the GreatestCommonDivisor method implementation with the one provided by .Net's inbuilt BigInteger struct.

--- Common/MathUtil.cs
+++ Common/MathUtil.cs
@@ -1,27 +1,13 @@
-using System;
+using System.Numerics;
 
 namespace Common
 {
     public static class MathUtils
     {
         /// <summary>
-        /// Use Euclid's algorithm to calculate the greatest common divisor (GCD) of two numbers.
-        /// From: http://csharphelper.com/blog/2014/08/calculate-the-greatest-common-divisor-gcd-and-least-common-multiple-lcm-of-two-integers-in-c/
+        /// Finds the greatest common divisor (GCD) of the two specified numbers.
         /// </summary>
-        public static long GreatestCommonDivisor(long a, long b)
-        {
-            a = Math.Abs(a);
-            b = Math.Abs(b);
-
-            // Pull out remainders.
-            for (; ; )
-            {
-                long remainder = a % b;
-                if (remainder == 0) return b;
-                a = b;
-                b = remainder;
-            }
-        }
+        public static long GreatestCommonDivisor(long a, long b) => (long)BigInteger.GreatestCommonDivisor(a, b);
 
         /// <summary>
         /// Returns the least common multiple (LCM) of the two specified numbers.
